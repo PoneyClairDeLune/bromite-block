fe5a232a48244d7bd7d602741688d7c88ef91e82
PoneyClairDeLune <88174309+PoneyClairDeLune@users.noreply.github.com>
2022-07-25 14:20:32 +0000

    Tests covers all now. Still server-side only.

--- src/deno/handler.js
+++ src/deno/handler.js
@@ -17,42 +17,40 @@ let handleRequest = function (request) {
 	console.info(`User agent object: ${JSON.stringify(uaStr)}`);
 	console.info(`User agent string: ${request.headers.get("user-agent")}`);
 	if (uaStr.agent == "Chrome") {
+		passed = 1;
 		// For full verdict, one must test presence of sec-ch-ua, sec-ch-ua-mobile and sec-ch-ua-platform
 		if (!request.headers.has("sec-ch-ua") && uaStr.agentVer[0] > 88) {
 			console.warn("Client hints stripped away intentionally.");
 			passed = 3;
 		};
-		if (uaStr.platformName == "Android") {
-			passed = 1;
-			// Version test
-			let equalsZero = 0;
-			uaStr.agentVer.forEach(function (e) {
-				if (e == 0) {
-					equalsZero ++;
-				};
-			});
-			if (equalsZero > 1) {
-				if (uaStr.agentVer[0] < 103) {
-					console.warn("Intentionally hiding version in user agent.");
-					passed = 2;
-				} else {
-					if (request.headers.has("sec-ch-ua")) {
-						if (request.headers.has("sec-ch-ua-full-version")) {
-							if (request.headers.get("sec-ch-ua-full-version").indexOf(".0.0") > -1) {
-								console.warn("Intentionally hiding version in client hints.");
-								passed = 2;
-							};
-						};
-					} else {
-						console.warn("No client hints available for versio detection.");
-						passed = 2;
-					};
-				};
+		// Version test
+		let equalsZero = 0;
+		uaStr.agentVer.forEach(function (e) {
+			if (e == 0) {
+				equalsZero ++;
 			};
-			if (uaStr.deviceModel.length < 4) {
-				console.warn("Illegal device model.");
+		});
+		if (equalsZero > 1) {
+			if (uaStr.agentVer[0] < 103) {
+				console.warn("Intentionally hiding version in user agent.");
 				passed = 2;
+			} else {
+				if (request.headers.has("sec-ch-ua")) {
+					if (request.headers.has("sec-ch-ua-full-version")) {
+						if (request.headers.get("sec-ch-ua-full-version").indexOf(".0.0") > -1) {
+							console.warn("Intentionally hiding version in client hints.");
+							passed = 2;
+						};
+					};
+				} else {
+					console.warn("No client hints available for versio detection.");
+					passed = 2;
+				};
 			};
+		};
+		if (uaStr.deviceModel.length < 4) {
+			console.warn("Illegal device model.");
+			passed = 2;
 		};
 	};
 	let response = wrapHtml(200, passState[passed], `${promptText[passed]}`.replace("${browser}", uaStr.agent));
